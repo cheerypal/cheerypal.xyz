236ab4c5bc8c68b28eea4877de04d9c88b2ad829
cheerypal <ejg9@hw.ac.uk>
2021-02-27 23:10:50 +0000

    projects started

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  var cards = document.getElementsByClassName("card");
+
+  cards[0].addEventListener("onmouseover", () => {
+    let overlay = document.getElementById("card-overlay");
+    overlay.style.display = "block";
+  });
+});
