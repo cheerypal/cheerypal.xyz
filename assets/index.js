0314b1d1243d01582e52e7da17ba3eb6fdae68c7
cheerypal <ejg9@hw.ac.uk>
2021-03-12 23:26:41 +0000

    layout and root css

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var cards = document.getElementsByClassName("card");
-
-  cards[0].addEventListener("onmouseover", () => {
-    let overlay = document.getElementById("card-overlay");
-    overlay.style.display = "block";
-  });
+  document.body.classList.remove("preload");
 });
