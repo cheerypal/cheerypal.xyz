6f6a79f5089574e5dd34f18f656cd1277763f2a5
cheerypal <ejg9@hw.ac.uk>
2021-03-20 01:10:53 +0000

    js

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.remove("preload");
+	// disable all transitions
+	document.body.classList.remove("preload");
+
+	//init localstorage
+
+	if (typeof Storage !== "undefined") {
+		if (!localStorage.languages)
+			localStorage.setItem("languages", JSON.stringify([]));
+	}
+
+	//github api
+	//get all the languages used by cheerypals github accounts
+	const getLanguages = () => {
+		let url = "https://api.github.com/users/cheerypal/repos";
+		fetch(url, { method: "GET" })
+			.then((response) => {
+				return response.json();
+			})
+			.then((jsonData) => {
+				console.log(jsonData);
+				let repos = [];
+				for (let i in jsonData) {
+					repos.push(jsonData[i].name);
+				}
+
+				for (let i in repos) {
+					let url =
+						"https://api.github.com/repos/cheerypal/" + repos[i] + "/languages";
+					fetch(url, { method: "GET" })
+						.then((response) => {
+							return response.json();
+						})
+						.then((jsonData) => {
+							console.log(jsonData);
+							let langs = JSON.parse(localStorage.getItem("languages"));
+							if (langs.length >= repos.length) {
+							} else {
+								localStorage.setItem("languages", JSON.stringify([]));
+								console.log(langs);
+								langs.push(jsonData);
+								localStorage.setItem("languages", JSON.stringify(langs));
+							}
+						});
+				}
+			});
+	};
+
+	getLanguages();
 });
